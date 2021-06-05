44c64e3cfc7b328079a1504ff584607cd88fcf13
Nita Andreea <nitaelenaandreea@gmail.com>
2021-06-05 17:12:02 +0300

    incercare esuata v2

--- src/main/java/com/project/cms/controller/InstructorController.java
+++ src/main/java/com/project/cms/controller/InstructorController.java
@@ -1,10 +1,12 @@
 package com.project.cms.controller;
 
 import com.project.cms.model.Course;
+import com.project.cms.model.Student;
 import com.project.cms.payload.request.CourseRegister;
 import com.project.cms.service.CourseService;
 import com.project.cms.service.FilesStorageService;
 import com.project.cms.service.InstructorService;
+import com.project.cms.service.StudentService;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,13 +23,15 @@ import java.util.Optional;
 public class InstructorController {
     private final CourseService courseService;
     private final InstructorService instructorService;
+    private final StudentService studentService;
     private final FilesStorageService filesStorageService;
     private final Mapper mapper;
 
     @Autowired
-    public InstructorController(CourseService courseService, InstructorService instructorService, FilesStorageService filesStorageService, Mapper mapper) {
+    public InstructorController(CourseService courseService, InstructorService instructorService, StudentService studentService, FilesStorageService filesStorageService, Mapper mapper) {
         this.courseService = courseService;
         this.instructorService = instructorService;
+        this.studentService = studentService;
         this.filesStorageService = filesStorageService;
         this.mapper = mapper;
     }
@@ -75,6 +79,37 @@ public class InstructorController {
             return ResponseEntity.notFound().build();
         filesStorageService.saveTimetable(timetable,course.get().getTimetable());
         return ResponseEntity.ok().build();
+    }
+    @PutMapping("/course/{id}/update")
+    public ResponseEntity<?> update(@PathVariable String id, @RequestBody CourseRegister courseRegister){
+        Optional<Course> course= courseService.findOne(id);
+        if(course.isEmpty())
+            return ResponseEntity.notFound().build();
+        course.get().setId(courseRegister.getId());
+        course.get().setName(courseRegister.getName());
+        course.get().setImageURL(courseRegister.getImageURL());
+        course.get().setCourseDuration(courseRegister.getCourseDuration());
+        course.get().setRegisterDuration(courseRegister.getRegisterDuration());
+       course.get().setAddress(courseRegister.getAddress());
+       course.get().setCity(courseRegister.getCity());
+       course.get().setCountry(courseRegister.getCountry());
+       return ResponseEntity.ok(courseService.save(course.get()));
+    }
+    @GetMapping("/student/{id}/cv")
+    public ResponseEntity<?> cvStudent(@PathVariable String id){
+        Optional<Student> student= studentService.findOne(id);
+        if(student.isEmpty())
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(filesStorageService.loadCv(student.get().getCv())) ;
+    }
+    @PutMapping("/course/{id}/post")
+    public ResponseEntity<?> post(@PathVariable String id,@RequestBody String post){
+        Optional<Course> course= courseService.findOne(id);
+        if(course.isEmpty())
+            return ResponseEntity.notFound().build();
+        course.get().addPost(post);
+        return ResponseEntity.ok(courseService.save(course.get()));
+
     }
 
 
